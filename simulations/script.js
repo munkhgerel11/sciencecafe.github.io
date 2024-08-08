dc4ca819d120b4e8f4abcff00e79530a731b9b6b
GerelPR <117137519+GerelPR@users.noreply.github.com>
2024-08-08 16:18:09 +0800

    added simulations

---
+++ simulations/script.js
@@ -0,0 +1,38 @@
+(function() {
+  const createDiv = (name, img_url, iframe_link) => {
+    const containerDiv = document.createElement('div');
+    containerDiv.className = 'list-item';
+    containerDiv.dataset.iframeLink = iframe_link; // attach iframe_link to the div object
+
+    const itemDiv = document.createElement('div');
+    itemDiv.className = 'item';
+    itemDiv.style.backgroundImage = `url('${img_url}')`;
+    containerDiv.appendChild(itemDiv);
+    
+    const span = document.createElement('span');
+    span.textContent = name;
+    containerDiv.appendChild(span);
+
+    containerDiv.addEventListener('click', function clicking(event){
+      iframe_link = event.target
+      const container = document.getElementsByClassName('container-iframe')[0];
+      container.innerHTML = `<iframe src="${this.dataset.iframeLink}" width="800px" height="600px" frameborder="1" allowfullscreen></iframe>`
+      window.scrollTo(0, 0);
+    });
+    return containerDiv;
+  };
+
+  fetch('phet_simulations_mn_with_iframe.json').then(response => response.json()).then(data => {
+      const container = document.getElementById('main');
+      data.forEach(item => {
+        const div = createDiv(item.name, item.img_url, item.iframe_url);
+        container.appendChild(div);
+      });
+    }).catch(error => console.error(error));
+
+  const container = document.getElementById('main');
+  const wrapper = document.getElementById('wrapper');
+  wrapper.appendChild(container);
+})();
+
+
